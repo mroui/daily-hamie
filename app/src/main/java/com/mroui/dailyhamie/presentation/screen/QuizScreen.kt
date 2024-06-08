ccf4a5b1e26441a4eec1bb3b2dc72cfcdb4a0145
Martyna Roj <martyna.roj@allegro.com>
2024-06-09 00:22:43 +0200

    Add disease 3 - button click effect

--- app/src/main/java/com/mroui/dailyhamie/presentation/screen/QuizScreen.kt
+++ app/src/main/java/com/mroui/dailyhamie/presentation/screen/QuizScreen.kt
@@ -1,5 +1,8 @@
 package com.mroui.dailyhamie.presentation.screen
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -9,10 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -27,7 +33,6 @@ internal fun QuizScreen(
     navController: NavController,
     quizResult: MutableMap<QuestionType, String> = mutableMapOf()
 ) {
-
     var questionIndex by remember { mutableIntStateOf(0) }
     val currentQuestion = QUESTIONS_DATA_SET[questionIndex]
 
@@ -50,6 +55,10 @@ internal fun QuizScreen(
             modifier = Modifier.padding(bottom = 16.dp)
         )
         currentQuestion.answers.forEach { answer ->
+
+            var isButtonPressed by remember { mutableStateOf(false) }
+            val buttonScale by animateFloatAsState(if (isButtonPressed) 0.9f else 1f)
+
             Button(
                 onClick = {
                     quizResult[currentQuestion.type] = answer
@@ -65,6 +74,21 @@ internal fun QuizScreen(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 16.dp)
+                    .graphicsLayer {
+                        scaleX = buttonScale
+                        scaleY = buttonScale
+                    }
+                    .pointerInput(isButtonPressed) {
+                        awaitPointerEventScope {
+                            isButtonPressed = if (isButtonPressed) {
+                                waitForUpOrCancellation()
+                                false
+                            } else {
+                                awaitFirstDown(false)
+                                true
+                            }
+                        }
+                    }
             ) {
                 Text(
                     modifier = Modifier.padding(8.dp),
